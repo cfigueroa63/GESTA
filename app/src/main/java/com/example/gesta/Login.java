ce88f57b39d1cdcf27acc18db864577a309a4b2f
iHYSTORY <cfigueroa@gmx.com>
2020-08-22 15:35:38 -0400

    Login

---
+++ app/src/main/java/com/example/gesta/Login.java
@@ -0,0 +1,22 @@
+package com.example.gesta;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void Login(View view){
+        Intent siguiente = new Intent(this, Main.class);
+        startActivity(siguiente);
+
+    }
+}
