2ff75e7d09610c299205c4f63b207acc1dd2f396
iHYSTORY <cfigueroa@gmx.com>
2020-08-27 00:05:00 -0400

    validacion login

--- app/src/main/java/com/example/gesta/Login.java
+++ app/src/main/java/com/example/gesta/Login.java
@@ -6,6 +6,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.gson.Gson;
+
 
 public class Login extends AppCompatActivity {
 
@@ -13,19 +17,30 @@ public class Login extends AppCompatActivity {
     private EditText pass;
 
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-      user= (EditText) findViewById(R.id.tv_user_nav);
-      pass = (EditText) findViewById(R.id.txt_pass);
+        user = (EditText) findViewById(R.id.tv_user_nav);
+        pass = (EditText) findViewById(R.id.txt_pass);
     }
 
-    public void Login(View view){
-        Intent login = new Intent(this, Main.class);
-       login.putExtra("data_user", user.getText().toString());
-        startActivity(login);
+    public void login(View view) {
+        if (user.getText().length() != 0 && pass.getText().length() != 0) {
+            if (autenticar()) {
+                Intent login = new Intent(this, Main.class);
+                login.putExtra("data_user", user.getText().toString());
+                startActivity(login);
+            } else {
+                Toast.makeText(this, "Usuario y/o contraseña incorrectas", Toast.LENGTH_LONG).show();
+            }
+        } else {
+            Toast.makeText(this, "Debe ingresar su usuario y contraseña", Toast.LENGTH_LONG).show();
+        }
     }
+
+    private boolean autenticar() {
+        return true;
+}
 }
